d6878f57c893631ac1fe60cddd0c21d84590941d
Marcelo Rojas PC <mrojasl@pucp.edu.pe>
2022-06-04 17:52:42 -0500

    listaOperadores jsp

--- src/main/java/com/example/cineestudiar/servlets/AdminServlet.java
+++ src/main/java/com/example/cineestudiar/servlets/AdminServlet.java
@@ -1,14 +1,29 @@
 package com.example.cineestudiar.servlets;
 
+import com.example.cineestudiar.beans.BUser;
+import com.example.cineestudiar.daos.AdminDao;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
-@WebServlet(name = "AdminServlet", value = "/AdminServlet")
+@WebServlet(name = "AdminServlet", urlPatterns = {"/AdminServlet",""})
 public class AdminServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try(PrintWriter printWriter = response.getWriter()) {
+            printWriter.println("Hola Mundo");
+        }
+        ArrayList<BUser> listaOperadores = AdminDao.obtenerOperadores();
+        request.setAttribute("listaOperadores",listaOperadores);
+        RequestDispatcher view = request.getRequestDispatcher("listaOperadores.jsp");
+        view.forward(request,response);
+
+
 
     }
 
